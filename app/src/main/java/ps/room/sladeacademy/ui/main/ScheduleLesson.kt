194cd7c8186eb39bbb477594ce7c0870262446b5
izowmart <izowmart8@gmail.com>
2020-10-08 17:33:07 +0300

    ui completed successfully

--- app/src/main/java/ps/room/sladeacademy/ui/main/ScheduleLesson.kt
+++ app/src/main/java/ps/room/sladeacademy/ui/main/ScheduleLesson.kt
@@ -1,12 +1,34 @@
 package ps.room.sladeacademy.ui.main
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_schedule_lesson.*
 import ps.room.sladeacademy.R
 
 class ScheduleLesson : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_schedule_lesson)
+
+        val toolbar = schedule_toolbar
+        toolbar.title = "Schedule Lesson"
+        setSupportActionBar(toolbar)
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            android.R.id.home -> {
+                this.finish()
+                true
+            }
+            else -> {
+                super.onOptionsItemSelected(item)
+            }
+        }
+
+
     }
 }
